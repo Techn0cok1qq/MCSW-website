0b310120914d1634f933fc370996488bbf86fd63
Techn0cok1qq <sonehaya53@gmail.com>
2026-07-20 10:49:28 +0900

    Add files via upload

--- js/pvp-tooltip.js
+++ js/pvp-tooltip.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const items = document.querySelectorAll(".kit-items img");
 
+  items.forEach(item => {
+    item.addEventListener("click", function(e) {
+
+      items.forEach(i => i.classList.remove("show-tooltip"));
+
+      item.classList.add("show-tooltip");
+    });
+  });
+
+  document.addEventListener("click", function(e) {
+    if (!e.target.closest(".kit-items img")) {
+      items.forEach(i => i.classList.remove("show-tooltip"));
+    }
+  });
+});
